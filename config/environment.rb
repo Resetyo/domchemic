a9e8c1a33486f2ab2c263dafe6a04780712ea433
Reset_yo <ostorodonodor@gmail.com>
2017-07-04 20:47:12 +0700

    locum

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-app_environment_variables = File.join(Rails.root, 'config', 'application.yml')
-load(app_environment_variables) if File.exists?(app_environment_variables)
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env].to_hash
