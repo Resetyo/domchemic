e83c408d17caa4a318a182afe9c660c39d4ef10b
Reset_yo <ostorodonodor@gmail.com>
2017-07-04 20:45:07 +0700

    locum

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+app_environment_variables = File.join(Rails.root, 'config', 'application.yml')
+load(app_environment_variables) if File.exists?(app_environment_variables)
