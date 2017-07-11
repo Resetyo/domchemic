1b86907579aa619b0e72466f60026d77cab30b5b
Reset_yo <ostorodonodor@gmail.com>
2017-07-11 15:11:27 +0700

    cron fix

--- config/initializers/process_file.rb
+++ config/initializers/process_file.rb
@@ -3,7 +3,7 @@ module ProcessFile
     path_to_file = Dir.glob("#{Rails.root}/public/uploads/products/*")[0]
     if path_to_file.present?
 
-      sleep 30
+      sleep 60
 
       xls = Roo::Excel.new(path_to_file)
       csv_text = xls.to_csv
@@ -11,9 +11,11 @@ module ProcessFile
 
       old_products = Product.all.pluck(:id)
 
-      csv.each_with_index do |row, index|
-        name = row[0][/\s.+(\/|\()/]
-        product = Product.create(name: name[1..-2], price: row[2].to_f) if name && row[2].present?
+      csv.each do |row|
+        if row[0].present? && row[2].present? && row[0] != "Наименование товара"
+          name = row[0][/\s.+(\/|\()/]
+        end
+        product = Product.create(name: name[1..-2], price: row[2].to_f) if name
       end
 
       Product.where("id in (?)", old_products).delete_all
