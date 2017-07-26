0e597477d85aac5f1a8ae3dc597cdae4a92328b9
Reset_yo <ostorodonodor@gmail.com>
2017-07-26 15:23:15 +0700

    change server

--- config/deploy/locum.rb
+++ config/deploy/locum.rb
@@ -1,6 +1,10 @@
-role :app, %w(hosting_ostorodonodor@neon.locum.ru)
-role :web, %w(hosting_ostorodonodor@neon.locum.ru)
-role :db, %w(hosting_ostorodonodor@neon.locum.ru)
+role :app, %w(hosting_domhimik@neon.locum.ru)
+role :web, %w(hosting_domhimik@neon.locum.ru)
+role :db, %w(hosting_domhimik@neon.locum.ru)
 
 set :ssh_options, forward_agent: true
 set :rails_env, :production
+
+# role :app, %w(hosting_ostorodonodor@neon.locum.ru)
+# role :web, %w(hosting_ostorodonodor@neon.locum.ru)
+# role :db, %w(hosting_ostorodonodor@neon.locum.ru)
