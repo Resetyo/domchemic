bae9b39e785aedf26c82552ec7f8ebcb7b312022
Victor <ostorodonodor@gmail.com>
2018-05-14 12:44:50 +0700

    Save products with transaction

--- config/initializers/process_file.rb
+++ config/initializers/process_file.rb
@@ -9,33 +9,35 @@ module ProcessFile
 
       old_products = Product.all.pluck(:id)
 
-      csv.each do |row|
-        if row[0].present? && row[2].present? && row[0] != "Наименование товара"
-          full_name = row[0]
-          name = row[0].gsub(/\(.+\)/,'')
-          name = name.gsub(/\/.+/,'') if name
-          code = row[0][/\A\S+\d/]
-        end
-        if name
-          product = Product.create( code: code, 
-                                    name: name[1..-2], 
-                                    full_name: full_name,
-                                    price: row[2].to_f
-                                    )
-
-          slug_code = SlugPreparatorRus.slug code, 'no-code'
-          image_folder = "#{Rails.root}/public/uploads/product/image/#{slug_code}"
-
-          if File.directory?(image_folder)
-            image_path = Dir.entries(image_folder).select do |i|
-              i[0..5] != "thumb_" && i.length > 4
-            end
-            image_path = "#{image_folder}/#{image_path[0]}"
-
-            product.image = File.open(image_path) if File.exist?(image_path)
+      Product.transaction do
+        csv.each do |row|
+          if row[0].present? && row[2].present? && row[0] != "Наименование товара"
+            full_name = row[0]
+            name = row[0].gsub(/\(.+\)/,'')
+            name = name.gsub(/\/.+/,'') if name
+            code = row[0][/\A\S+\d/]
           end
+          if name
+            product = Product.create( code: code, 
+                                      name: name[1..-2], 
+                                      full_name: full_name,
+                                      price: row[2].to_f
+                                      )
 
-          product.save
+            slug_code = SlugPreparatorRus.slug code, 'no-code'
+            image_folder = "#{Rails.root}/public/uploads/product/image/#{slug_code}"
+
+            if File.directory?(image_folder)
+              image_path = Dir.entries(image_folder).select do |i|
+                i[0..5] != "thumb_" && i.length > 4
+              end
+              image_path = "#{image_folder}/#{image_path[0]}"
+
+              product.image = File.open(image_path) if File.exist?(image_path)
+            end
+
+            product.save
+          end
         end
       end
 
