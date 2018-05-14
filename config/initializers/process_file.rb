05e6cb233a5ff201c8485c2d8fcef6f862755398
Victor <ostorodonodor@gmail.com>
2018-05-14 14:47:58 +0700

    check

--- config/initializers/process_file.rb
+++ config/initializers/process_file.rb
@@ -2,6 +2,7 @@ module ProcessFile
   def self.update
     path_to_file = Dir.glob("#{Rails.root}/public/uploads/products/*")[0]
     if path_to_file.present?
+      puts "#{DateTime.now} file found"
 
       xls = Roo::Excel.new(path_to_file)
       csv_text = xls.to_csv
